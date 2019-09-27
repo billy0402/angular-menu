d6819929704f838dce7798427190550ac26d616c
Billy Huang <10646003@ntub.edu.tw>
2019-09-27 21:04:44 +0800

    components - init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageComponent } from './components/page/page.component';
+import { IndexComponent } from './components/index/index.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { RoyalComponent } from './components/royal/royal.component';
+import { OrderDetailComponent } from './components/order-detail/order-detail.component';
+import { CertComponent } from './components/cert/cert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    IndexComponent,
+    NavBarComponent,
+    RoyalComponent,
+    OrderDetailComponent,
+    CertComponent
   ],
   imports: [
     BrowserModule,
